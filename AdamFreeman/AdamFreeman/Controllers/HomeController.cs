e6a9cc7390578025dc40b4504a8b299a828dcfa2
Ashish Patel <ashishpatel626@lavabit.com>
2022-08-24 16:57:07 -0400

    Added Adam Freeman exercises

---
+++ AdamFreeman/AdamFreeman/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using AdamFreeman.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AdamFreeman.Controllers
+{
+  public class HomeController : Controller
+  {
+    public ViewResult Index()
+    {
+      int hour = DateTime.Now.Hour;
+      string viewModel = hour < 12 ? "Good Morning" : "Good Afternoon";
+      return View("MyView", viewModel);
+    }
+  }
+}
---
+++ PartyInvites.sln/PartyInvites.sln/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartyInvites.sln.Models;
+using System.Diagnostics;
+
+namespace PartyInvites.sln.Controllers
+{
+  public class HomeController : Controller
+  {
+    public IActionResult Index()
+    {
+      return View();
+    }
+  }
+}
